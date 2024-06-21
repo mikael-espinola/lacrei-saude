84eaa7e2930c19b7dc63a52e49132a5c573f837b
mikael-espinola <mikaelespinolaa@gmail.com>
2024-06-21 16:52:57 -0300

    feat: responsive layout

--- src/components/header/style.tsx
+++ src/components/header/style.tsx
@@ -14,14 +14,13 @@ export const Container = styled.section`
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  padding: 1.8em;
-  width: 90%;
-  margin: 0 auto;
+  padding: 0.4em;
+  padding: 1.5em 0 0.5em 0;
 
-  @media (max-width: ${breakpoints.mobile}) {
-    width: 100%;
-    padding: 0.4em;
-    padding: 1.5em 0 0.5em 0;
+  @media (min-width: ${breakpoints.mobile}) {
+    width: 90%;
+    margin: 0 auto;
+    padding: 1.8em;
   }
 `;
 export const MenuButtons = styled.div`
@@ -37,6 +36,10 @@ export const LogoContainer = styled.div`
   display: flex;
   width: 50%;
   justify-content: center;
+
+  @media (min-width: ${breakpoints.mobile}) {
+    justify-content: start;
+  }
 `;
 
 export const Logo = styled(Image)`
